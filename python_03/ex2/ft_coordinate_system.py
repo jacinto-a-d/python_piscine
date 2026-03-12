3ff4dd178aa10e210947cf6f06f576094d8123eb
Jacinto Abad Diaz <jabad-di@student.42malaga.com>
2026-03-12 20:06:58 +0100

    ff

--- python_03/ex2/ft_coordinate_system.py
+++ python_03/ex2/ft_coordinate_system.py
@@ -4,10 +4,10 @@
 #                                                          :::      ::::::::  #
 #   ft_coordinate_system.py                              :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
-#   By: dipekko <dipekko@student.42.fr>              +#+  +:+       +#+       #
+#   By: jabad-di <jabad-di@student.42malaga.com>     +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/03/11 16:44:12 by dipekko             #+#    #+#            #
-#   Updated: 2026/03/12 11:42:44 by dipekko            ###   ########.fr      #
+#   Updated: 2026/03/12 18:25:45 by jabad-di           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -24,40 +24,45 @@ def parseo_coordinate(data: str) -> tuple[int]:
             int(parts[1]),
             int(parts[2])
         )
-        print(f"Position created: {coords}")
         return coords
-    except IndexError:
-        pass
 
     except ValueError as e:
-        print(f"Parsing invalid coordinates:{sys.argv[1]}")
+        print(f"Parsing invalid coordinates: \"{sys.argv[1]}\"")
         print(f"Error parsing coordinates: {e}")
+        #no me deja utilizar type
+        print(f"Error details - Type: ValueError, Args: {e.args}")
         return None
 
 
-def distance_coords(coordi: tuple[int]) -> None:
+def distance_coords(p1: tuple[int, int, int], p2: tuple[int, int, int] = (0, 0, 0)) -> None:
     """x"""
-    if coordi:
-        dist: float = math.sqrt(coordi[0]**2 + coordi[1]**2 + coordi[2]**2)
-        print(f"Distance between: (0, 0, 0) and {coordi}: {dist:.2f}")
+    if p1:
+        dist: float = math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+        print(f"Distance between: {p2} and {p1}: {dist:.2f}")
 
 
 def main() -> None:
     """x"""
     print("=== Game Coordinate System ===\n")
-    coords_firts: tuple[int] = parseo_coordinate("10, 20, 5")
+    coords_firts: tuple[int, int, int] = parseo_coordinate("10, 20, 5")
+    print(f"Position created: {coords_firts}")
     distance_coords(coords_firts)
     print("")
 
-    print(f"Parsing coordinates: {sys.argv[1]}")
-    coordinates: tuple[int] = parseo_coordinate(sys.argv[1])
-    distance_coords(coordinates)
-    print("")
-
-    print("Unpacking demonstration:")
-    x, y, z = coordinates
-    print(f"Player at x={x}, y={y}, z={z}")
-    print(f"Coordinates: X={x}, Y={y}, Z={z}")
+    try:
+        coordinates: tuple[int, int, int] = parseo_coordinate(sys.argv[1])
+        print(f"Parsing coordinates: \"{sys.argv[1]}\"")
+        print(f"Position created: {coordinates}")
+        distance_coords(coordinates)
+        print("")
+        
+        if coordinates:
+            print("Unpacking demonstration:")
+            x, y, z = coordinates
+            print(f"Player at x={x}, y={y}, z={z}")
+            print(f"Coordinates: X={x}, Y={y}, Z={z}")
+    except IndexError:
+        pass
 
 
 if __name__ == "__main__":
